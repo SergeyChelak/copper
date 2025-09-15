924a3c447f7777b87a1fd93c52400935dce15c77
Sergey Chelak <sergey.chelak@gmail.com>
2025-09-15 21:48:59 +0300

    minimal rust kernel + nix shell

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static TEXT: &[u8] = b"Copper started";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in TEXT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0F;
+        }
+    }
+
     loop {}
 }
 
