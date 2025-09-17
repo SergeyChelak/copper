6b214058b02536ef3ed876c7ea21e5de06a17d3c
Sergey Chelak <sergey.chelak@gmail.com>
2025-09-18 01:31:12 +0300

    integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,28 +4,15 @@
 #![test_runner(copper::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use copper::{halt_loop, println};
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
-    halt_loop()
+    loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    halt_loop()
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     copper::test_panic_handler(info)
