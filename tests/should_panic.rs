8c6e7e8acdce047728f2c0e3a611d3d3d190d2b3
Sergey Chelak <sergey.chelak@gmail.com>
2025-09-18 22:52:53 +0300

    harness test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use copper::{QemuExitCode, exit_qemu, halt_loop, serial_print, serial_println};
 use core::panic::PanicInfo;
@@ -16,21 +13,14 @@ fn panic_handler(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test didn't panic]");
+    exit_qemu(QemuExitCode::Failed);
+
     halt_loop()
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test didn't panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
+#[allow(clippy::assertions_on_constants)]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert!(false);
